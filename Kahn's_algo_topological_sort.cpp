5f763214ac407ab6e13439ced15c02f518ee440a
minhdeptraisomotthegioi <101634652+Trminh06@users.noreply.github.com>
2022-04-12 18:48:35 +0700

    Update Kahn's_algo_topological_sort.cpp

--- Kahn's_algo_topological_sort.cpp
+++ Kahn's_algo_topological_sort.cpp
@@ -85,3 +85,80 @@ int main(){
     return 0;
 }
 
+//COURSE SCHEDULE
+// we need to mix both of them to get AC in this problem
+// actually i don't know why, maybe i was wrong somewhere
+// Kahn algorithm can not check DAG so I have to use dfs to check that sh*t
+// after checking the graph is DAG, I do the Kahn
+// Here is the code
+/*
+    #include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e5 + 1;
+int n, num[maxn], indegree[maxn];
+vector <int> a[maxn];
+queue <int> ans;
+void dfs(int u){
+    num[u] = 1;
+    for(int v : a[u])
+        if (!num[v]){
+            num[u] = 1;
+            dfs(v);
+        }else
+            if (num[v] == 1){
+                cout << "IMPOSSIBLE";
+                exit(0);
+            }
+    num[u] = 2;
+}
+void Kahn(){
+    queue <int> q;
+    for(int i = 1; i <= n; ++i)
+        if (!indegree[i]){
+            q.push(i);
+            ans.push(i);
+        }
+    if (!q.size()){
+        cout << "IMPOSSIBLE";
+        exit(0);
+    }
+    while (q.size()){
+        int u = q.front();
+        q.pop();
+        for(int v : a[u]){
+            indegree[v]--;
+            if (!indegree[v]){
+                q.push(v);
+                ans.push(v);
+            }
+        }
+    }
+    if (!ans.size()){
+        cout << "IMPOSSIBLE";
+        exit(0);
+    }
+    while (ans.size()){
+        cout << ans.front() << " ";
+        ans.pop();
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int m, u, v;
+    cin >> n >> m;
+    fill_n(indegree, 0, n);
+    while (m--){
+        cin >> u >> v;
+        indegree[v]++;
+        a[u].push_back(v);
+    }
+    fill_n(num, 0, n);
+    for(int i = 1; i <= n; ++i)
+        if (!num[i])
+            dfs(i);
+    Kahn();
+    return 0;
+}
+
+*/
