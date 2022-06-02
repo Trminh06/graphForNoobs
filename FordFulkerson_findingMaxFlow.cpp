0508d0efad61fd83feefe7f9625ae32d3135796e
minhdeptraisomotthegioi <101634652+Trminh06@users.noreply.github.com>
2022-06-02 14:55:01 +0700

    Update FordFulkerson_findingMaxFlow.cpp

--- FordFulkerson_findingMaxFlow.cpp
+++ FordFulkerson_findingMaxFlow.cpp
@@ -1,46 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fi first
-#define se second
-typedef pair<int,int> ii;
+typedef long long ll;
 const int maxn = 1e3 + 1;
-const int oo = 1e9 + 7;
-int n, s, t, trace[maxn], c[maxn][maxn], f[maxn][maxn], d[maxn], ans = 0;
+const ll oo = 1e18;
+int n, s, t, trace[maxn];
+ll f[maxn][maxn], ans = 0;
+bool vis[maxn];
 vector <int> a[maxn];
 bool bfs(){
     for(int i = 1; i <= n; ++i)
-        d[i] = trace[i] = 0;
+        vis[i] = 0;
     queue <int> q;
     q.push(s);
-    d[s] = 1;
+    vis[s] = 1;
+    trace[s] = -1;
     while (q.size()){
         int u = q.front();
         q.pop();
         for(int v : a[u]){
-            if (d[v] || f[u][v] >= c[u][v])
+            if (vis[v] || f[u][v] <= 0)
                 continue;
+            vis[v] = 1;
             trace[v] = u;
-            d[v] = 1;
-            if (v == t)
-                return 1;
             q.push(v);
         }
     }
-    return trace[t];
+    return vis[t];
 }
 void FordFulkerson(){
-    int minn = oo;
     int u, v = t;
+    ll minn = oo;
     while (v != s){
         u = trace[v];
-        minn = min(minn, c[u][v] - f[u][v]);
+        minn = min(minn, f[u][v]);
         v = u;
     }
-    v = t;
+    v =  t;
     while (v != s){
         u = trace[v];
-        f[u][v] += minn;
-        f[v][u] -= minn;
+        f[u][v] -= minn;
+        f[v][u] += minn;
         v = u;
     }
     ans += minn;
@@ -48,17 +47,17 @@ void FordFulkerson(){
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int m, u, v, w;
+    int m, u, v;
+    ll c;
     cin >> n >> m >> s >> t;
     while (m--){
-        cin >> u >> v >> w;
-        c[u][v] = w;
+        cin >> u >> v >> c;
+        f[u][v] = c;
         a[u].push_back(v);
         a[v].push_back(u);
     }
     while (bfs())
         FordFulkerson();
-
     cout << ans;
     return 0;
 }
